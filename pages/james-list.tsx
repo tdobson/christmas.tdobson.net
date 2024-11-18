c2d1e06aef751cbb8f45a8c0c086c173451b171e
Tim Dobson <hello@tdobson.net>
2024-11-18 13:55:41 +0000

    feat: Update James's list with priority, other, and do-not-buy items

--- pages/james-list.tsx
+++ pages/james-list.tsx
@@ -1,36 +1,74 @@
-import { Title, List, Stack, Card, Text } from '@mantine/core';
+import { Title, List, Stack, Card, Text, Alert } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons-react';
 
 export default function JamesListPage() {
+  const priorityItems = [
+    'Teeny weenie balance bike (smallest available size - currently size 3.5 feet)',
+    'Thicker foam/rubber play mat for safety under climbing areas',
+    'Kids\' food preparation knives',
+    'Animal posters for his room (especially dogs)',
+    'Number floor tiles/mats'
+  ];
+
+  const otherItems = [
+    'Warm boots (size 4-4.5)',
+    'Climbing harness (Edelrid model)',
+    'Small buoyancy aid for swimming',
+    'Orange(s) for Christmas stocking',
+    'Flyaway footballs'
+  ];
+
+  const doNotBuy = [
+    'Clothes (except specified boots)',
+    'Coats',
+    'Wellies',
+    'Books',
+    'Media',
+    'Dummies',
+    'Duplo/Lego (have plenty)',
+    'Chocolate/sweets'
+  ];
+
   return (
     <Stack gap="md">
       <Title order={1}>James's Christmas List</Title>
 
+      <Alert icon={<IconAlertCircle size="1rem" />} title="Important Note" color="red">
+        Please check with parents before purchasing to avoid duplicates
+      </Alert>
+
       <Card withBorder>
         <Stack gap="sm">
-          <Title order={2}>Toys & Games</Title>
+          <Title order={2}>Priority Items</Title>
           <List>
-            <List.Item>LEGO Star Wars sets</List.Item>
-            <List.Item>Remote control car</List.Item>
-            <List.Item>Nintendo Switch games</List.Item>
-            <List.Item>Science experiment kit</List.Item>
+            {priorityItems.map((item) => (
+              <List.Item key={item}>{item}</List.Item>
+            ))}
           </List>
         </Stack>
       </Card>
 
       <Card withBorder>
         <Stack gap="sm">
-          <Title order={2}>Books & Learning</Title>
+          <Title order={2}>Other Items</Title>
           <List>
-            <List.Item>Dinosaur encyclopedia</List.Item>
-            <List.Item>Magic Tree House series books</List.Item>
-            <List.Item>Space-themed picture books</List.Item>
+            {otherItems.map((item) => (
+              <List.Item key={item}>{item}</List.Item>
+            ))}
           </List>
         </Stack>
       </Card>
 
-      <Text size="sm" c="dimmed">
-        Note: This list will be updated as new items are added.
-      </Text>
+      <Card withBorder>
+        <Stack gap="sm">
+          <Title order={2} c="red">Please Do Not Buy</Title>
+          <List>
+            {doNotBuy.map((item) => (
+              <List.Item key={item}>{item}</List.Item>
+            ))}
+          </List>
+        </Stack>
+      </Card>
     </Stack>
   );
 }
