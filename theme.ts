b0c4e883ed02184015481529ab2a29cac5d2c867
Tim Dobson <hello@tdobson.net>
2024-11-18 13:52:55 +0000

    feat: Add Christmas-themed color palette and component styling to theme

--- theme.ts
+++ theme.ts
@@ -1,5 +1,44 @@
-import { createTheme } from '@mantine/core';
+import { createTheme, rem } from '@mantine/core';
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: 'green',
+  colors: {
+    christmas: [
+      '#EDFBF0',
+      '#D4F0DA',
+      '#A9E1B6',
+      '#7ED292',
+      '#53C36E',
+      '#28B449',
+      '#1F903B',
+      '#166C2C',
+      '#0D481D',
+      '#04240F',
+    ],
+  },
+  components: {
+    Card: {
+      defaultProps: {
+        padding: 'lg',
+        radius: 'md',
+      },
+    },
+    Title: {
+      styles: {
+        root: {
+          '&[data-order="1"]': {
+            marginBottom: rem(20),
+          },
+          '&[data-order="2"]': {
+            marginBottom: rem(12),
+          },
+        },
+      },
+    },
+    Stack: {
+      defaultProps: {
+        spacing: 'md',
+      },
+    },
+  },
 });
