b294b00059caebabccb59bf179dbbaab21dae8bd
Tim Dobson <hello@tdobson.net>
2024-11-18 14:13:02 +0000

    feat: Add gift-giving guidelines alert to James list page

--- pages/james-list.tsx
+++ pages/james-list.tsx
@@ -42,6 +42,19 @@ export default function JamesListPage() {
 				Please check with parents before purchasing to avoid duplicates
 			</Alert>
 
+			<Alert
+				icon={<IconAlertCircle size="1rem" />}
+				title="Gift-Giving Guidelines"
+				color="blue"
+			>
+				<List>
+					<List.Item>Second-hand items are welcomed and encouraged</List.Item>
+					<List.Item>Practical/useful items preferred over decorative</List.Item>
+					<List.Item>Help with tasks/time is often more valuable than physical items</List.Item>
+					<List.Item>Consider helping with research/planning when giving money towards items</List.Item>
+				</List>
+			</Alert>
+
 			<Card withBorder>
 				<Stack gap="sm">
 					<Title order={2}>Priority Items</Title>
